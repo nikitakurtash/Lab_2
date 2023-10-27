b1aa898186b54dc82530b9acb49014ee562baa64
nikitakurtash <nikitakurtash@gmail.com>
2023-10-28 00:57:29 +0300

    quicksort

--- file.go
+++ file.go
@@ -1,15 +1,37 @@
 package main
 
-func findKthLargest(nums []int, k int) int {
-	for i := 0; i < len(nums)-1; i++ {
-		for j := 0; j < len(nums)-1-i; j++ {
-			if nums[j] > nums[j+1] {
-				tmp := nums[j]
-				nums[j] = nums[j+1]
-				nums[j+1] = tmp
-			}
+import (
+	"fmt"
+)
+
+func quickSort(nums []int) []int {
+	if len(nums) < 2 {
+		return nums
+	}
+
+	pivot := nums[0]
+	var less, greater []int
+	for _, num := range nums[1:] {
+		if num <= pivot {
+			less = append(less, num)
+		} else {
+			greater = append(greater, num)
 		}
 	}
-	return nums[len(nums)-k]
+
+	result := append(quickSort(less), pivot)
+	result = append(result, quickSort(greater)...)
+	return result
 }
 
+func findKthLargest(nums []int, k int) int {
+	sortedNums := quickSort(nums)
+	return sortedNums[len(sortedNums)-k]
+}
+
+func main() {
+	nums := []int{5, 2, 4, 1, 3}
+	k := 3
+	fmt.Println()
+	fmt.Println(findKthLargest(nums, k))
+}
