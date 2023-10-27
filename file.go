46c870a7cd1e4ee86486729567964bc66bb7ea2f
nikitakurtash <nikitakurtash@gmail.com>
2023-10-28 00:39:52 +0300

    s

---
+++ file.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func findKthLargest(nums []int, k int) int {
+	for i := 0; i < len(nums)-1; i++ {
+		for j := 0; j < len(nums)-1-i; j++ {
+			if nums[j] > nums[j+1] {
+				tmp := nums[j]
+				nums[j] = nums[j+1]
+				nums[j+1] = tmp
+			}
+		}
+	}
+	return nums[len(nums)-k]
+}
+
