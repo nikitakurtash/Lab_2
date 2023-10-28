96a0147201f8c8655a77c1cba24d20beb3a7ec7b
nikitakurtash <nikitakurtash@gmail.com>
2023-10-29 01:27:57 +0300

    sdf

--- file.go
+++ file.go
@@ -1,15 +1,13 @@
 package main
 
-func shellSort(nums []int) {
+func findKthLargest(nums []int, k int) int {
 	n := len(nums)
 	h := 1
-	// Находим начальное значение h
 	for h < n/3 {
 		h = 3*h + 1
 	}
 
 	for h >= 1 {
-		// Производим вставку с инкрементом h
 		for i := h; i < n; i++ {
 			for j := i; j >= h && nums[j] < nums[j-h]; j -= h {
 				nums[j], nums[j-h] = nums[j-h], nums[j]
@@ -17,9 +15,5 @@ func shellSort(nums []int) {
 		}
 		h /= 3
 	}
-}
-
-func findKthLargest(nums []int, k int) int {
-	shellSort(nums)
 	return nums[len(nums)-k]
 }
